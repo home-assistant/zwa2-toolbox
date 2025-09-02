52f7a73db1a92886d912949ea10bb9aefb160375
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-09-02 21:48:10 +0200

    chore: publish to Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "/zwa2-toolbox/",
 	css: {
 		postcss: './postcss.config.cjs',
 	},
