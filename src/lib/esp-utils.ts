cba521dc7d623df4da79c0727a992b7f73334867
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-09-04 15:42:54 +0200

    wait for command menu to show up and retrieve version info

--- src/lib/esp-utils.ts
+++ src/lib/esp-utils.ts
@@ -5,7 +5,6 @@ import { wait } from "alcalzone-shared/async";
  * Utility functions for ESP-specific operations
  */
 
-
 const MAGIC_BAUDRATES = [150, 300, 600];
 
 /**
@@ -17,7 +16,6 @@ const MAGIC_BAUDRATES = [150, 300, 600];
 export async function enterESPBootloader(
 	serialPort: SerialPort,
 ): Promise<boolean> {
-
 	const disconnectPromise = createDeferredPromise<void>();
 	function onDisconnect() {
 		console.log("Serial port disconnected, likely entered bootloader mode");
@@ -26,34 +24,79 @@ export async function enterESPBootloader(
 
 	try {
 		// When entering the bootloader, the ZWA-2 disconnects and the ESP bootloader device reconnects
-		await serialPort.close();
+		// await serialPort.close();
 		serialPort.addEventListener("disconnect", onDisconnect);
 
-		for (const baudrate of MAGIC_BAUDRATES) {
-			await serialPort.open({ baudRate: baudrate });
-			await wait(100);
+		for (let i = 0; i < MAGIC_BAUDRATES.length; i++) {
+			const baudrate = MAGIC_BAUDRATES[i];
+			if (i > 0) {
+				await wait(100);
+			}
 			await serialPort.close();
+			await serialPort.open({ baudRate: baudrate });
+		}
+		console.log("Sent magic baudrate sequence");
+
+		const reader = serialPort.readable?.getReader();
+		if (!reader) {
+			console.error(
+				"Failed to get readable stream from serial port. Probably means we entered bootloader.",
+			);
+			return true;
 		}
 
-		console.log("Sent magic baudrate sequence");
+		const awaitChunk = (predicate: (chunk: string) => boolean) => {
+			return reader
+				.read()
+				.then(({ value, done }) => {
+					// Return true only if we've received the expected chunk.
+					const receivedChunk =
+						value && new TextDecoder().decode(value);
+						if (done || receivedChunk == undefined) return undefined;
+						if (!predicate(receivedChunk)) return undefined;
+						return receivedChunk;
+				})
+				.catch(() => undefined);
+		};
+
+		const cmdMenuPromise = awaitChunk((chunk) => chunk.startsWith("cmd>"));
 
 		// In the legacy implementation, the magic sequence already triggers the bootloader
 		const legacyResult = await Promise.race([
 			disconnectPromise.then(() => true),
+			// A positive result of the cmdMenuPromise means we've seen the cmd menu, so we did NOT enter bootloader yet
+			cmdMenuPromise.then((result) => !result),
 			wait(2000).then(() => false),
 		]);
 		if (legacyResult) return true;
 
-		// The newer implementation enters command mode instead. We need to send `BE` to enter the ESP bootloader
-		await serialPort.open({ baudRate: 115200 });
+		// The newer implementation enters command mode instead.
+		// We'll retrieve the version info first (unused for now),
+		// and then send `BE` to enter the ESP bootloader
 		const writer = serialPort.writable?.getWriter();
 		if (!writer) {
 			throw new Error("Failed to get writable stream from serial port");
 		}
-		const data = new TextEncoder().encode("BE");
-		await writer.write(data);
-		writer.releaseLock();
+
+		const writeCommand = async (cmd: string) => {
+			const command = new TextEncoder().encode(cmd);
+			await writer.write(command);
+		};
+
+		const infoPromise = awaitChunk(() => true);
+		await writeCommand("I");
+		console.log("Sent 'I' to get version info");
+		const info = await infoPromise;
+		if (info) {
+			console.log("Received version info:", info);
+		} else {
+			console.warn("Did not receive version info");
+		}
+
+		await writeCommand("BE");
 		console.log("Sent 'BE' to enter bootloader");
+
+		writer.releaseLock();
 
 		// Wait up to 5 seconds for the disconnect event
 		const result = await Promise.race([
@@ -61,7 +104,6 @@ export async function enterESPBootloader(
 			wait(5000).then(() => false),
 		]);
 		return result;
-
 	} catch (error) {
 		console.error("Failed to enter ESP bootloader:", error);
 		return false;
